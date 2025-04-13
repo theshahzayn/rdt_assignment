f10806d4dc6292643ab8d4737abc6b757452e988
Shahzain Zaidi <143355259+theshahzayn@users.noreply.github.com>
2025-04-13 18:01:09 +0500

    FSM

--- config.py
+++ config.py
@@ -1,17 +1,13 @@
 
 # Configuration File
 
-# Packet properties
-TOTAL_PACKETS = 5        # Total number of packets to send
-PACKET_SIZE = 64         # Size of each packet (not used directly but kept for reference)
+TOTAL_PACKETS = 5       
+PACKET_SIZE = 64        
 
-# Network properties (probabilities)
-LOSS_PROBABILITY = 0.2      # Probability of packet loss
-CORRUPT_PROBABILITY = 0.1   # Probability of packet corruption
-DELAY_PROBABILITY = 0.1     # Probability of packet delay
+LOSS_PROBABILITY = 0.2     
+CORRUPT_PROBABILITY = 0.1   
+DELAY_PROBABILITY = 0.1    
 
-# GBN & SR settings
 WINDOW_SIZE = 4
 
-# Timeout period (in seconds)
 TIMEOUT = 2
